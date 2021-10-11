959cbca812c15d34cd5a58b882a5c7b5c5f7a0c3
Alanhsiu <alan901101@gmail.com>
2021-10-11 18:23:02 +0800

    v2

--- hw2/main.js
+++ hw2/main.js
@@ -5,31 +5,37 @@ for (var i = 0; i < 3; i++) {
 var totalNumber = 0;
 var nameOfAlbum = new Array();
 nameOfAlbum[0] = "landscape";
+nameOfAlbum[1] = "flower";
+nameOfAlbum[2] = "people";
 album[0] = [
-    "https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-    "https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "https://images.pexels.com/photos/38326/pexels-photo-38326.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "https://images.pexels.com/photos/1450360/pexels-photo-1450360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "https://images.pexels.com/photos/33582/sunrise-phu-quoc-island-ocean.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "https://images.pexels.com/photos/464336/pexels-photo-464336.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "https://images.pexels.com/photos/1114896/pexels-photo-1114896.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/2113566/pexels-photo-2113566.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/2440024/pexels-photo-2440024.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/460621/pexels-photo-460621.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/707915/pexels-photo-707915.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/1297790/pexels-photo-1297790.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/4101555/pexels-photo-4101555.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
 ]
 album[1] = [
-    "https://images.pexels.com/photos/1083822/pexels-photo-1083822.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "https://images.pexels.com/photos/136255/pexels-photo-136255.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "https://images.pexels.com/photos/701816/pexels-photo-701816.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "https://images.pexels.com/photos/4622892/pexels-photo-4622892.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    "https://images.pexels.com/photos/1005711/pexels-photo-1005711.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/1038002/pexels-photo-1038002.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/714918/pexels-photo-714918.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/1169084/pexels-photo-1169084.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
 ]
 album[2] = [
-    "https://images.pexels.com/photos/2577274/pexels-photo-2577274.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    "https://images.pexels.com/photos/2577274/pexels-photo-2577274.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "https://images.pexels.com/photos/6333511/pexels-photo-6333511.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/6454959/pexels-photo-6454959.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
 ]
+album[3] = []
 // album[0].pop();
 
 // alert("album length : " + album.length);
 // alert("album[0] length : " + album[0].length);
 // alert("album[0] length : " + album[1].length);
 let curAlbum = 0;
+let prevAlbum = -1;
 let curIdx = 0;
 let prevIdx = -1;
 let displayImg = document.getElementById("displayImg");
@@ -40,10 +46,10 @@ let num = document.getElementById("num");
 
 setName();
 setPhotos();
-setDisplay(curIdx);
+setDisplay();
 
 function setName() {
-    document.getElementById("albumName").innerText = nameOfAlbum[0];
+    document.getElementById("albumName").innerText = nameOfAlbum[curAlbum];
 }
 
 function clearPhotos() {
@@ -67,10 +73,14 @@ function setDisplay() {
 }
 
 function setMargin() {
-    let curMargin = document.getElementById("photo" + String(curIdx));
-    let prevMargin = document.getElementById("photo" + String(prevIdx));
-    curMargin.style = "border: 0.3em solid #00D3B6;border-style:groove;border-radius: 0.3em;";
-    prevMargin.style = "border: ;";
+    document.getElementById("photo" + String(curIdx)).style = "border: 0.3em solid #00D3B6;border-style:groove;border-radius: 0.3em;";
+    document.getElementById("album" + String(curAlbum)).style = "border: 0.3em solid #00D3B6;border-style:groove;border-radius: 0.3em;";
+    if(prevIdx!=-1){
+        document.getElementById("photo" + String(prevIdx)).style = "border: ;";
+    }
+    if(prevAlbum!=-1){
+        document.getElementById("album" + String(prevAlbum)).style = "border: ;";
+    }
 }
 function prevImg() {
     if (curIdx > 0) {
@@ -80,7 +90,7 @@ function prevImg() {
     }
 }
 function nextImg() {
-    if (curIdx < album[0].length - 1) {
+    if (curIdx < album[curAlbum].length - 1) {
         prevIdx = curIdx;
         ++curIdx;
         setDisplay();
@@ -95,9 +105,11 @@ function clickImg(index) {
 function checkIdx() {
     if (curIdx === 0) {
         prevButton.classList.add("disabled");
+        nextButton.classList.remove("disabled");
     }
-    else if (curIdx === album[0].length - 1) {
+    else if (curIdx === album[curAlbum].length - 1) {
         nextButton.classList.add("disabled");
+        prevButton.classList.remove("disabled");
     }
     else {
         prevButton.classList.remove("disabled");
@@ -110,11 +122,23 @@ function checkState() {
 }
 
 function changeAlbum(index) {
-    clearPhotos();
-    curAlbum = index;
-    curIdx = 0;
-    prevIdx = -1;
-    setName();
-    setPhotos();
-    setDisplay(curIdx);
+    if(!checkEmpty(index)){
+        clearPhotos();
+        prevAlbum =curAlbum;
+        curAlbum = index;
+        curIdx = 0;
+        prevIdx = -1;
+        setName();
+        setPhotos();
+        setDisplay();
+    }
+}
+function checkEmpty(index){
+    if(album[index].length===0){
+        alert("此為空相簿");
+        return true;
+    }
+    else{
+        return false;
+    }
 }
