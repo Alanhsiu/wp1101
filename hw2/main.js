9fe67e9c66c1b645c79cc17555a89fa0b633d933
Alan Hsieh <b09901066@ntu.edu.tw>
2021-10-18 14:25:06 +0800

    v2

--- hw2/main.js
+++ hw2/main.js
@@ -1,13 +1,26 @@
+for (var i = 1; i < 9; i++) {
+        var articleNode = document.createElement("article");
+        var divNode = document.createElement("div");
+        var imgNode = document.createElement("img");
+        imgNode.src = album[curAlbum][i];
+        divNode.id = "photo" + i;
+        divNode.onclick = clickImg(i);
+        divNode.appendChild(imgNode);
+        articleNode.appendChild(divNode);
+        previewNode,appendChild(articleNode);
+    }
+
+let previewNode = document.getElementById("preview");
+var numOfAlbum = 0;
+var nameOfAlbum = new Array();
+
 var album = new Array();
 for (var i = 0; i < 3; i++) {
     album.push(new Array());
 }
-var totalNumber = 0;
-var nameOfAlbum = new Array();
-nameOfAlbum[0] = "landscape";
-nameOfAlbum[1] = "flower";
-nameOfAlbum[2] = "people";
+
 album[0] = [
+    "landscape",
     "https://images.pexels.com/photos/1114896/pexels-photo-1114896.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     "https://images.pexels.com/photos/2113566/pexels-photo-2113566.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     "https://images.pexels.com/photos/2440024/pexels-photo-2440024.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
@@ -15,20 +28,23 @@ album[0] = [
     "https://images.pexels.com/photos/707915/pexels-photo-707915.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     "https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     "https://images.pexels.com/photos/1297790/pexels-photo-1297790.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    "https://images.pexels.com/photos/4101555/pexels-photo-4101555.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    // "https://images.pexels.com/photos/4101555/pexels-photo-4101555.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
 ]
 album[1] = [
+    "flower",
     "https://images.pexels.com/photos/1005711/pexels-photo-1005711.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     "https://images.pexels.com/photos/1038002/pexels-photo-1038002.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     "https://images.pexels.com/photos/714918/pexels-photo-714918.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     "https://images.pexels.com/photos/1169084/pexels-photo-1169084.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
 ]
 album[2] = [
-    "https://images.pexels.com/photos/2577274/pexels-photo-2577274.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "people",
     "https://images.pexels.com/photos/6333511/pexels-photo-6333511.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     "https://images.pexels.com/photos/6454959/pexels-photo-6454959.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
 ]
-album[3] = []
+album[3] = [
+    "empty"
+]
 // album[0].pop();
 
 // alert("album length : " + album.length);
@@ -36,7 +52,7 @@ album[3] = []
 // alert("album[0] length : " + album[1].length);
 let curAlbum = 0;
 let prevAlbum = -1;
-let curIdx = 0;
+let curIdx = 1;
 let prevIdx = -1;
 let displayImg = document.getElementById("displayImg");
 let prevButton = document.getElementById("prev");
@@ -44,25 +60,29 @@ let nextButton = document.getElementById("next");
 let num = document.getElementById("num");
 
 
-setName();
-setPhotos();
-setDisplay();
+start();
+
+function start() {
+    setName();
+    setPhotos();
+    setDisplay();
+}
 
 function setName() {
-    document.getElementById("albumName").innerText = nameOfAlbum[curAlbum];
+    document.getElementById("albumName").innerText = album[curAlbum][0];
 }
 
 function clearPhotos() {
     let curMargin = document.getElementById("photo" + String(curIdx));
     curMargin.style = "border: ;";
-    for (var i = 0; i < album[curAlbum].length; i++) {
-        document.getElementById("photo" + String(i)).firstChild.src = "";
+    for (var i = 1; i < album[curAlbum].length; i++) {
+        document.getElementById("photo" + i).firstChild.src = "";
     }
 }
 
 function setPhotos() {
-    for (var i = 0; i < album[curAlbum].length; i++) {
-        document.getElementById("photo" + String(i)).firstChild.src = album[curAlbum][i];
+    for (var i = 1; i < album[curAlbum].length; i++) {
+        document.getElementById("photo" + i).firstChild.src = album[curAlbum][i];
     }
 }
 function setDisplay() {
@@ -75,15 +95,15 @@ function setDisplay() {
 function setMargin() {
     document.getElementById("photo" + String(curIdx)).style = "border: 0.3em solid #00D3B6;border-style:groove;border-radius: 0.3em;";
     document.getElementById("album" + String(curAlbum)).style = "border: 0.3em solid #00D3B6;border-style:groove;border-radius: 0.3em;";
-    if(prevIdx!=-1){
+    if (prevIdx != -1) {
         document.getElementById("photo" + String(prevIdx)).style = "border: ;";
     }
-    if(prevAlbum!=-1){
+    if (prevAlbum != -1) {
         document.getElementById("album" + String(prevAlbum)).style = "border: ;";
     }
 }
 function prevImg() {
-    if (curIdx > 0) {
+    if (curIdx > 1) {
         prevIdx = curIdx;
         --curIdx;
         setDisplay();
@@ -103,7 +123,7 @@ function clickImg(index) {
 }
 
 function checkIdx() {
-    if (curIdx === 0) {
+    if (curIdx === 1) {
         prevButton.classList.add("disabled");
         nextButton.classList.remove("disabled");
     }
@@ -118,27 +138,59 @@ function checkIdx() {
 }
 
 function checkState() {
-    document.getElementById("num").innerText = (curIdx + 1) + '/' + album[curAlbum].length;
+    document.getElementById("num").innerText = (curIdx) + '/' + (album[curAlbum].length - 1);
 }
 
 function changeAlbum(index) {
-    if(!checkEmpty(index)){
+    if (!checkEmpty(index)) {
         clearPhotos();
-        prevAlbum =curAlbum;
+        prevAlbum = curAlbum;
         curAlbum = index;
-        curIdx = 0;
+        curIdx = 1;
         prevIdx = -1;
         setName();
         setPhotos();
         setDisplay();
     }
 }
-function checkEmpty(index){
-    if(album[index].length===0){
+function checkEmpty(index) {
+    if (album[index].length === 1) {
         alert("此為空相簿");
         return true;
     }
-    else{
+    else {
         return false;
+    }
+}
+
+function addPhoto() {
+    var link = prompt("請輸入相片連結", "url");
+    if (link != null && link != "") {
+        album[curAlbum].push(link);
+        start();
+    }
+}
+function deletePhoto(){
+    clearPhotos();
+    album[curAlbum].splice(curIdx,1);
+    changeAlbum(curAlbum);
+}
+function addAlbum() {
+    var albumName = prompt("請輸入相簿名稱", "albumName");
+    if (albumName != null && albumName != "") {
+        newAlbum(albumName);
+    }
+}
+
+let albumNode = document.getElementById("album");
+class newAlbum {
+    constructor(albumName) {
+        this.name = albumNum;
+        this.albumNum = ++numOfAlbum;
+        this.buttonNode = document.createElement("button");
+        this.buttonNode.textContent = `${albumName}`;
+        this.buttonNode.id = "album" + albumNum;
+        this.buttonNode.onclick = "changeAlbum(0)";
+        albumNode.appendChild(buttonNode);
     }
 }
