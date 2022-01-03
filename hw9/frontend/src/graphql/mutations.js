1c851dc87618e6e54adba6b641d26e2ab811e94a
Alan Hsieh <b09901066@ntu.edu.tw>
2022-01-03 19:15:01 +0800

    v3-1

--- hw9/frontend/src/graphql/mutations.js
+++ hw9/frontend/src/graphql/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_CHATBOX_MUTATION = gql`
-  mutation createChatBox($name1: String!, $name2: String!) {
+  mutation createChatBox($name1: String, $name2: String) {
     createChatBox(name1: $name1, name2: $name2) {
       name
       messages{
