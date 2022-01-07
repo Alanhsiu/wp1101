7661327cc835decbb3de43341f29dcf0c0044dfa
Gabriel <jimmy111923@gmail.com>
2022-01-07 19:36:59 +0800

    merged

--- final/backend/src/routes/index.js
+++ final/backend/src/routes/index.js
@@ -3,8 +3,33 @@ import createResume from "./api/createResume";
 import deleteDB from "./api/deleteDB";
 import Teacher from "../models/teacher";
 import Parent from "../models/parent";
+import uuid from "node-uuid";
+import session from "express-session";
+import bcrypt from "bcrypt";
+import fetch from "node-fetch";
+import mongoose from "mongoose";
+const MongoStore = require("connect-mongo")(session);
+
+import { User } from "../models/User";
+import { loginRequired } from "./api/middleware";
 
 const router = express.Router();
+const secret = uuid.v4();
+const sessionOptions = {
+  cookie: {
+    path: "/",
+    httpOnly: true,
+    secure: true,
+    maxAge: null,
+  },
+  resave: false,
+  saveUninitialized: false,
+  secret,
+  unset: "destroy",
+  store: new MongoStore({ url: process.env.MONGO_URL }),
+};
+
+sessionOptions.store.clear();
 
 router.get("/query-all", async (_, res) => {
   const existing = await Teacher.find().sort({});
@@ -38,6 +63,7 @@ router.get("/query-cards", async (req, res) => {
   const queryType = req.query.type;
   const queryString = req.query.queryString;
   let query;
+<<<<<<< HEAD
   if (queryType == "name") {query = await Teacher.find({ name: queryString });
   console.log("ok")
   console.log(query)}
@@ -46,12 +72,62 @@ router.get("/query-cards", async (req, res) => {
   console.log(query)}
   if (query.length !== 0) res.send({ message: query });
   else res.send({message:`${queryType} (${queryString}) not found!`});
+=======
+  if (queryType == "name") query = await Teacher.find({ name: queryString });
+  else query = await Teacher.find({ subject: queryString });
+  var results = new Array();
+  for (let i = 0; i < query.length; i++)
+    results[
+      i
+    ] = `Exist (${query[i].name}, ${query[i].subject}, ${query[i].price})`;
+
+  if (query.length !== 0) res.send({ messages: results });
+  else res.send({ message: `${queryType} (${queryString}) not found!` });
 });
 router.delete("/clear-db", async (_, res) => {
   const msg = await deleteDB();
   res.send({ message: msg });
 });
 
+export default router;
+router.get("/session", loginRequired, async (req, res, next) => {
+  res.status(200).send({
+    userID: req.session.userID,
+  });
+});
 
+router.post(
+  "session",
+  express.urlencoded({ extended: false }),
+  async (req, res, next) => {
+    const { userID, password } = req.body;
+    if (!userID || !password) {
+      res.status(400).end();
+      return;
+    }
+    const user = await User.findOne({ userID }).exec();
+    if (!user) {
+      res.status(400).end();
+      return;
+    }
+    const hashedPwd = user.password;
+    const { name } = user;
 
-export default router; 
+    const match = await bcrypt.compare(password, hashedPwd);
+    if (!match) {
+      res.status(401).end();
+      return;
+    }
+
+    req.session.userID;
+    req.session.name = name;
+    res.status(200).send({ userID });
+  }
+);
+
+router.delete("session", async (req, res, next) => {
+  req.session = null;
+  res.status(204).end();
+});
+
+export default router;
