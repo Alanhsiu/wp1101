68edb22519d5c4d5b0ca943499aa084034c0c8ea
Ferayer <ferayerhychen@gmail.com>
2022-01-07 20:43:18 +0800

    pretest

---
+++ final/backend/src/resolvers/subject.js
@@ -0,0 +1,15 @@
+const SubjectResolver = {
+    Math :"Math",
+    Mandarin :"Mandarin",
+    English:"English",
+    Pyhsics:"Pyhsics",
+    Chemistry:"Chemistry",
+    Biology:"Biology",
+    Geography:"Geography",
+    History:"History",
+    Circuit:"Circuit",
+    Pokemon:"Pokemon"
+  };
+  
+  export default StatusResolver;
+  
